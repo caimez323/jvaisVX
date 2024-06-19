fb8c2edfcbcbe19ade7adfd93aae7bb90353be28
unknown <caimezae@gmail.com>
2024-06-18 22:39:44 -0400

    add combo streak on grosse maille feature

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import discord
 import os
 import random
+from datetime import datetime,timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+
 emojiList = [":muscle:",":thumbsup:",":punch:",":fist:",":left_facing_fist:",":right_facing_fist:",":ok_hand:",":call_me:",":index_pointing_at_the_viewer:",":mechanical_arm:",":leg:",":clap:",":handshake:",":sunglasses:"]
-triggerWordMailleList = ["gros","grosse","maille","muscle", "musclé","maillon","biceps","abdos","fort","force"]
-mailleIncrement = ["Double" , "Triple", "Quadruple","Quintuple","Sextuple","Septuple","Octuple","Nonuple","Décuple","Impressionnante","Épique","Dévastatrice","Brutale","Imprévisible","Soudaine","Fulgurante","Impeccable","Dominatrice","Inarrêtable"]
-
-
+triggerWordMailleList = ["gros","maille","muscle", "musclé","maillon","biceps","abdos","fort","force"]
+mailleIncrement = ["","","Double" , "Triple", "Quadruple","Quintuple","Sextuple","Septuple","Octuple","Nonuple","Décuple","Impressionnante","Épique","Dévastatrice","Brutale","Imprévisible","Soudaine","Fulgurante","Impeccable","Dominatrice","Inarrêtable"]
+mailleTimeCount = datetime.now()
+mailleTimeLimit = 60
+mailleCombo = 0
 client = discord.Client(intents=intents)
 
 reputation_dict = {}
@@ -42,12 +45,23 @@ async def on_message(message):
         else:
             reputation_dict[message_author_name]=1
         
-
     elif "vxtwitter" in message.content or "fixvx" in message.content:
         await message.add_reaction('\N{THUMBS UP SIGN}')
 
     if any(word in str(message.content) for word in triggerWordMailleList) or any(word in str(message.content).lower() for word in triggerWordMailleList):
-        await message.reply("Grosse Maille {}".format(random.choice(emojiList)),mention_author=False)
+        global mailleTimeCount
+        global mailleCombo
+        if mailleCombo == 0 :
+            mailleTimeCount = datetime.now()
+        else:
+            if (datetime.now() - mailleTimeCount) < timedelta(seconds=(7 if mailleCombo == 1 else int(mailleTimeLimit/mailleCombo))):
+                mailleTimeCount = datetime.now()
+            else:
+                mailleCombo = -1
+        if mailleCombo < len(mailleIncrement)-1:
+            mailleCombo+=1
+        
+        await message.reply("{}Grosse Maille {}".format((mailleIncrement[mailleCombo]+(" " if mailleCombo!= 0 else "")),random.choice(emojiList)),mention_author=False)
 
 
     if message.content.startswith(bot_prefix):
