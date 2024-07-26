c9601dcafaab782b954c900330f7af65648b212c
Clément T <52360173+caimez323@users.noreply.github.com>
2024-07-26 14:13:11 -0400

    Remove Maille and add instagram support

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from datetime import datetime,timedelta
 intents = discord.Intents.default()
 intents.message_content = True
 
-
-emojiList = [":muscle:",":thumbsup:",":punch:",":fist:",":left_facing_fist:",":right_facing_fist:",":ok_hand:",":call_me:",":index_pointing_at_the_viewer:",":mechanical_arm:",":leg:",":clap:",":handshake:",":sunglasses:"]
-triggerWordMailleList = ["gros","maille","muscle", "musclé","maillon","biceps","abdos","fort","force"]
-mailleIncrement = ["","","Double" , "Triple", "Quadruple","Quintuple","Sextuple","Septuple","Octuple","Nonuple","Décuple","Impressionnante","Épique","Dévastatrice","Brutale","Imprévisible","Soudaine","Fulgurante","Impeccable","Dominatrice","Inarrêtable"]
-mailleTimeCount = datetime.now()
-mailleTimeLimit = 60
-mailleCombo = 0
 client = discord.Client(intents=intents)
 
 reputation_dict = {}
@@ -36,8 +29,12 @@ async def on_message(message):
     if ('https://x.com/') in message.content:
         await message.channel.send(message.content.replace("https://x.com/","https://fixvx.com/") + " (" + message.author.name + ")")
         await message.delete()
-    
-    if "//vxtwitter.com/" in message.content or  "//fixvx.com/" in message.content:
+        
+    if ("http://instagram.com/") in message.content:
+        await message.channel.send(message.content.replace("http://intsagram.com/","http://ddinstagram.com/") + " (" + message.author.name + ")")
+        await message.delete()
+        
+    if "//vxtwitter.com/" in message.content or  "//fixvx.com/" in message.content or "http://ddinstagram.com/" in message.content:
         await message.add_reaction('♥')
         message_author_name = message.author.name
 
@@ -48,25 +45,6 @@ async def on_message(message):
         
     elif "vxtwitter" in message.content or "fixvx" in message.content:
         await message.add_reaction('\N{THUMBS UP SIGN}')
-
-    if any(word in str(message.content) for word in triggerWordMailleList) or any(word in str(message.content).lower() for word in triggerWordMailleList):
-        global mailleTimeCount
-        global mailleCombo
-        if mailleCombo == 0 :
-            mailleTimeCount = datetime.now()
-        else:
-            if (datetime.now() - mailleTimeCount) < timedelta(seconds=(7 if mailleCombo == 1 else int(mailleTimeLimit/mailleCombo))):
-                mailleTimeCount = datetime.now()
-            else:
-                mailleCombo = -1
-        if mailleCombo < len(mailleIncrement)-1:
-            mailleCombo+=1
-        
-        await message.reply("{}Grosse Maille {}".format((mailleIncrement[mailleCombo]+(" " if mailleCombo!= 0 else "")),random.choice(emojiList)),mention_author=False)
-        #await asyncio.sleep(random.uniform(23,250))
-        #botMessage = await message.channel.send(f'{message.author.mention}')
-        #await asyncio.sleep(1)
-        #await botMessage.delete()
 
     if message.content.startswith(bot_prefix):
 
