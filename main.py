92ba5d8b52d0f871ff82e23bc59aed84696d01da
caimez323 <caimezae@gmail.com>
2024-08-03 16:16:56 +0200

    remove unsued

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
 import discord
 import os
 
-class VXBot(discord.Client):
-    def __init__(self, intents=None):
-        super().__init__(intents=intents)
-        self.reputation_dict = {}
-        self.bot_prefix = "§"
+intents = discord.Intents.default()
+intents.message_content = True
 
-    async def on_ready(self):
-        print('We have logged in as {0.user}'.format(self))
+client = discord.Client(intents=intents)
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
+reputation_dict = {}
 
-        if 'https://twitter.com/' in message.content:
-            await message.channel.send(message.content.replace("https://twitter.com/", "https://vxtwitter.com/") + " (" + message.author.name + ")")
-            await message.delete()
+bot_prefix = "§"
 
-        if 'https://x.com/' in message.content:
-            await message.channel.send(message.content.replace("https://x.com/", "https://fixvx.com/") + " (" + message.author.name + ")")
-            await message.delete()
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-        if "https://www.instagram.com/" in message.content:
-            await message.channel.send(message.content.replace("https://www.instagram.com/", "https://www.ddinstagram.com/") + " (" + message.author.name + ")")
-            await message.delete()
 
-        if "//vxtwitter.com/" in message.content or "//fixvx.com/" in message.content or "https://ddinstagram.com/" in message.content:
-            await message.add_reaction('♥')
-            message_author_name = message.author.name
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-            if message_author_name in self.reputation_dict:
-                self.reputation_dict[message_author_name] += 1
-            else:
-                self.reputation_dict[message_author_name] = 1
+    if ('https://twitter.com/') in message.content :
+        await message.channel.send(message.content.replace("https://twitter.com/","https://vxtwitter.com/") + " (" + message.author.name + ")")
+        await message.delete()
 
-        elif "vxtwitter" in message.content or "fixvx" in message.content:
-            await message.add_reaction('\N{THUMBS UP SIGN}')
+    if ('https://x.com/') in message.content:
+        await message.channel.send(message.content.replace("https://x.com/","https://fixvx.com/") + " (" + message.author.name + ")")
+        await message.delete()
+        
+    if ("https://www.instagram.com/") in message.content:
+        await message.channel.send(message.content.replace("https://www.instagram.com/","https://www.ddinstagram.com/") + " (" + message.author.name + ")")
+        await message.delete()
+        
+    if "//vxtwitter.com/" in message.content or  "//fixvx.com/" in message.content or "https://ddinstagram.com/" in message.content:
+        await message.add_reaction('♥')
+        message_author_name = message.author.name
 
-        if message.content.startswith(self.bot_prefix):
-            if message.content[1:] == "reputation":
-                if not self.reputation_dict:
-                    await message.channel.send("Personne n'est dans le leaderboard pour le moment...")
-                    return
-                buffer = ""
-                toCrown = True
-                for user, score in dict(sorted(self.reputation_dict.items(), key=lambda x: x[1])).items():
-                    buffer += " | {}{} : {} |\n".format(":crown: " if toCrown else "", user, score)
-                    if toCrown:
-                        toCrown = False
-                await message.channel.send(buffer)
+        if(message_author_name in reputation_dict):
+            reputation_dict[message_author_name]+=1
+        else:
+            reputation_dict[message_author_name]=1
+        
+    elif "vxtwitter" in message.content or "fixvx" in message.content:
+        await message.add_reaction('\N{THUMBS UP SIGN}')
 
-# Ensure the module can be used as a standalone script
-if __name__ == "__main__":
-    intents = discord.Intents.default()
-    intents.message_content = True
-    bot = VXBot(intents=intents)
+    if message.content.startswith(bot_prefix):
 
-    try:
-        token = os.getenv("DISCORD_TOKEN") or ""
-        bot.run(token)
-    except discord.HTTPException as e:
-        raise e
+        if message.content[1:] == "reputation":
+            if (not any(reputation_dict)):
+                await message.channel.send("Personne n'est dans le leaderboard pour le moment...")
+                return
+            buffer = ""
+            toCrown = True
+            for user,score in dict(sorted(reputation_dict.items(), key=lambda x:x[1])).items():
+                buffer += " | {}{} : {} |\n".format(":crown: " if toCrown else "",user,score)
+                if toCrown : toCrown = False
+            await message.channel.send(buffer)
+           
+
+try:
+  token = os.getenv("DISCORD_TOKEN") or ""
+  client.run(token)
+except discord.HTTPException as e:
+    raise e
